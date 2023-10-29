b64f90aca9f95d59fc3a17022cf840e058f778ed
Nico Gilardoni <nicogilardonik@gmail.com>
2023-10-29 12:21:28 -0300

    rich text installed

--- src/components/Acknowledgment/Acknowledgment.vue
+++ src/components/Acknowledgment/Acknowledgment.vue
@@ -3,11 +3,23 @@
     <h2>RECONOCIMIENTO</h2>
   </div>
   
+  <QuillEditor theme="snow" />
+
+
 </template>
 
 <script>
+
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
 export default {
   name: 'Acknowledgment',
+
+  components: {
+    QuillEditor
+  },
+
   data() {
     return {
      
