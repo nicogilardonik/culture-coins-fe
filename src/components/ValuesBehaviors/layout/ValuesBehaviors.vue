8b593fc6a39db787d28d61505c52665bfe21a24f
joaquin610 <joaquinbanchero610@hotmail.com>
2023-11-29 00:20:33 -0300

    menu y sub menu de valores

---
+++ src/components/ValuesBehaviors/layout/ValuesBehaviors.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>aca</h1>
+</template>
+<script>
+export default {
+    data() {
+    return {
+      title: 'Values and behaviors',
+      isMobile: window.innerWidth <= 768,
+    }
+  },
+    mounted() {
+    window.addEventListener('resize', this.checkWindowSize);
+    this.checkWindowSize();
+    this.setTitle();
+  },
+    methods: {
+    setTitle() {
+      this.$store.commit('setPageTitle', this.title);
+    },
+    checkWindowSize() {
+      this.isMobile = window.innerWidth <= 768;
+    },
+}
+}
+</script>
+<style scoped>
+</style>
+
+
+
+
+
