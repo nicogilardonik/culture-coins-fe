a0de64ec6e68323f073a2fbb2c2b2302aeebc647
joaquin-banchero_cencosud <joaquinbanchero610@hotmail.com>
2024-01-15 00:29:28 -0300

    con estilo

--- src/components/MiInfogain/Communities/layaout/CommunityCard.vue
+++ src/components/MiInfogain/Communities/layaout/CommunityCard.vue
@@ -1,60 +1,79 @@
 <template>
-    <div>
-        <h2>{{ name }}</h2>
-        <ul>
-           <li v-for="user in UsersFilter" :key="user.id">{{ user.nickName }}</li>
-        </ul>
-        <button @click="save">save</button>
-        
-    </div>
+  <CCol :sm="12" :md="12" class="p-1">
+    <CCard>
+      <CCardHeader class="d-flex justify-content-between">
+        <div class="d-flex align-self-center">
+          <h4 class="d-inline-block">{{ name }}</h4>
+        </div>
+      </CCardHeader>
+      <CCardBody>
+        <CRow>
+          <CCol sm="6" md="6" lg="8" xl="10">
+            <div>
+              <div class="custom-display">
+                <p>members:</p>
+                <ul>
+                  <li v-for="user in UsersFilter" :key="user.id">{{ user.nickName }}</li>
+                </ul>
+              </div>
+            </div>
+          </CCol>
+
+          <CCol sm="6" md="6" lg="4" xl="2">
+            <CButton color="primary" size="md" class="kibana-font-weight" @click="save">
+              join
+            </CButton>
+          </CCol>
+
+        </CRow>
+      </CCardBody>
+    </CCard>
+  </CCol>
 </template>
 
 <script>
 import CommunitiesService from '@/components/MiInfogain/Communities/services/CommunitiesService';
-//import CustomEditor from '@/components/CustomEditor.vue';
 
 export default {
-    props: {
-        name: { type: String },
-        users: { type: Array },
-    },
-//    components: {
-//        CustomEditor
-//    },
-    data() {
+  props: {
+    name: { type: String },
+    users: { type: Array },
+  },
+  data() {
     return {
       UsersFilter: [],
 
     }
   },
-    mounted() {
+  mounted() {
     this.setUsers();
   },
   methods: {
     setUsers() {
-            this.UsersFilter = this.users.filter(user => user.communities.includes(this.name));
-        },
-    async save(){
-        if (!this.userProfile.communities.includes(this.name)) {
+      this.UsersFilter = this.users.filter(user => user.communities.includes(this.name));
+    },
+    async save() {
+      if (!this.userProfile.communities.includes(this.name)) {
         this.userProfile.communities.push(this.name);
         try {
-      await CommunitiesService.update(this.userProfile).then(() => {
-        this.showSuccess('Entro a la comunidad.');
-      })
-        .catch((error) => {
+          await CommunitiesService.update(this.userProfile).then(() => {
+            this.showSuccess('Entro a la comunidad.');
+          })
+            .catch((error) => {
+              this.showError(error.error ?? error);
+            });
+        } catch (error) {
           this.showError(error.error ?? error);
-        });
-    } catch (error) {
-      this.showError(error.error ?? error);
-    }
-}else {
-      this.$swal.fire({
-        title: 'Community already added!',
-        text: 'You have already added this community to your profile.',
-        icon: 'warning',
-        confirmButtonText: 'OK',
-      })
-    }},
+        }
+      } else {
+        this.$swal.fire({
+          title: 'Community already added!',
+          text: 'You have already added this community to your profile.',
+          icon: 'warning',
+          confirmButtonText: 'OK',
+        })
+      }
+    },
     showError(text) {
       this.$swal.fire({
         title: "Error!",
@@ -62,24 +81,22 @@ export default {
         icon: "error",
         confirmButtonText: "Ok",
       });
-    },   
+    },
     showSuccess(text) {
-    this.$swal.fire({
-      title: 'Success!',
-      text: text,
-      icon: 'success',
-      confirmButtonText: 'Ok',
-    });
-  }, 
-},
-computed: {
+      this.$swal.fire({
+        title: 'Success!',
+        text: text,
+        icon: 'success',
+        confirmButtonText: 'Ok',
+      });
+    },
+  },
+  computed: {
     userProfile() {
       return this.$store.state.userProfile;
     },
-}
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
