c83c0a1438b4c4407ee3151d9163fbbcfee6dfe2
joaquin-banchero_cencosud <joaquinbanchero610@hotmail.com>
2024-02-15 00:02:21 -0300

    areglar user filter en la card

--- src/components/MiInfogain/Communities/layaout/CommunityCard.vue
+++ src/components/MiInfogain/Communities/layaout/CommunityCard.vue
@@ -20,10 +20,10 @@
           </CCol>
 
           <CCol sm="6" md="6" lg="4" xl="2">
-            <CButton v-if="userInComunity" color="primary" size="md" class="kibana-font-weight" @click="chage">
+            <CButton v-if="userInComunity" color="primary" class="kibana-font-weight" @click="change">
               Leave
             </CButton>
-            <CButton v-else color="primary" size="md" class="kibana-font-weight" @click="chage">
+            <CButton v-else color="primary" class="kibana-font-weight" @click="change">
               Join
             </CButton>
           </CCol>
@@ -40,6 +40,7 @@ import CommunitiesService from '@/components/MiInfogain/Communities/services/Com
 export default {
   props: {
     name: { type: String },
+    users: { type: Array },
   },
   data() {
     return {
@@ -58,49 +59,65 @@ export default {
         this.userInComunity = true;
       }
     },
-
-    async setUsers() {
-      try {
-        await CommunitiesService.getUsers().then((response) => {
-          this.UsersFilter = response.filter(user => user.communities.includes(this.name))
-        }).catch((error) => {
-          this.showError(error.error ?? error);
-        });
-      } catch (error) {
-        this.showError(error.error ?? error);
-      }
-      ;
+    setUsers() {
+      //console.log("0");
+      const userCard = this.users.filter(user => user.communities.includes(this.name))
+      this.UsersFilter = userCard
+       if (this.name == ".NET") {
+      //   console.log("primer console.log");
+      //   console.log("1", this.users);
+         console.log("UsersFilter en setUsers", this.UsersFilter);
+       }
     },
-    async chage() {
+
+    // async setUsers() {
+    //   try {
+    //     await CommunitiesService.getUsers().then((response) => {
+    //       this.UsersFilter = response.filter(user => user.communities.includes(this.name))
+    //     }).catch((error) => {
+    //       this.showError(error.error ?? error);
+    //     });
+    //   } catch (error) {
+    //     this.showError(error.error ?? error);
+    //   }
+    //   ;
+    // },
+
+    async change() {
+      debugger
+      console.log("UsersFilter en change" ,this.UsersFilter);
+      console.log("UsersFilter en change 2" ,this.UsersFilter);
       let mensaje;
       let userComunity;
       if (!this.userProfile.communities.includes(this.name)) {
         this.userProfile.communities.push(this.name);
         mensaje = "I join the community.";
         userComunity = true;
-      }else{
+      } else {
         mensaje = "I left the community.";
         userComunity = false;
         const index = this.userProfile.communities.indexOf(this.name);
         this.userProfile.communities.splice(index, 1);
       }
       try {
-          await CommunitiesService.update(this.userProfile).then(() => {
-            this.showSuccess(mensaje);
-            this.userInComunity = userComunity;
-            if (this.userInComunity) {
-              this.UsersFilter.push(this.userProfile);
-            }else{
-              const userIndex = this.UsersFilter.indexOf(this.userProfile);
-              this.UsersFilter.splice(userIndex, 1);
-            }
-          })
-            .catch((error) => {
-              this.showError(error.error ?? error);
-            });
-        } catch (error) {
-          this.showError(error.error ?? error);
-        }
+        await CommunitiesService.update(this.userProfile).then(() => {
+          this.showSuccess(mensaje);
+          this.userInComunity = userComunity;
+          if (this.userInComunity) {
+            this.UsersFilter.push(this.userProfile);
+          } else {
+            const userIndex = this.UsersFilter.indexOf(this.userProfile);
+            console.log(userIndex);
+            console.log("userIndex", this.UsersFilter[userIndex]);
+            this.UsersFilter.splice(userIndex, 1);
+          }
+        })
+          .catch((error) => {
+            this.showError(error.error ?? error);
+          });
+      } catch (error) {
+        this.showError(error.error ?? error);
+      }
     },
     showError(text) {
       this.$swal.fire({
